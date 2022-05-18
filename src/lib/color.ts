b8368d36751ffc9b6f294ffa0428534fff255119
maanlamp <32102935+maanlamp@users.noreply.github.com>
2022-05-18 20:33:22 +0200

    Prepare for hosting

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -13,9 +13,9 @@ const rgbToHsl = ([r, g, b]: readonly [
 		s = 0,
 		l = 0;
 
-	if (delta == 0) h = 0;
-	else if (cmax == r) h = ((g - b) / delta) % 6;
-	else if (cmax == g) h = (b - r) / delta + 2;
+	if (delta === 0) h = 0;
+	else if (cmax === r) h = ((g - b) / delta) % 6;
+	else if (cmax === g) h = (b - r) / delta + 2;
 	else h = (r - g) / delta + 4;
 
 	h = Math.round(h * 60);
@@ -24,7 +24,9 @@ const rgbToHsl = ([r, g, b]: readonly [
 
 	l = (cmax + cmin) / 2;
 	s =
-		delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+		delta === 0
+			? 0
+			: delta / (1 - Math.abs(2 * l - 1));
 	s = +(s * 100).toFixed(1);
 	l = +(l * 100).toFixed(1);
 
@@ -35,11 +37,11 @@ const hexToRgb = (hex: string) => {
 	let r: any = 0,
 		g: any = 0,
 		b: any = 0;
-	if (hex.length == 4) {
+	if (hex.length === 4) {
 		r = "0x" + hex[1] + hex[1];
 		g = "0x" + hex[2] + hex[2];
 		b = "0x" + hex[3] + hex[3];
-	} else if (hex.length == 7) {
+	} else if (hex.length === 7) {
 		r = "0x" + hex[1] + hex[2];
 		g = "0x" + hex[3] + hex[4];
 		b = "0x" + hex[5] + hex[6];
